038b1265710ffac99bd2e570ecbe5c2ecab2ef87
Jacob Hooey <jhooey@gmail.com>
2013-08-12 16:20:11 -0400

    Created a procedure to house the basic search code

--- index.py
+++ index.py
@@ -1,7 +1,15 @@
+
 page =('<div id="top_bin"><div id="top_content" class="width960">'
 '<div class="udacity float-left"><a href= "http://udacity.com">')
-anchor_string = '<a'
-href_String = 'href='
-start_link_quote = page.find('"', page.find(href_String, page.find(anchor_string)))
-end_link_quote = page.find('"', start_link_quote + 1)
-print (page[start_link_quote + 1 : end_link_quote])  
+
+#finds the position of a " (left quote) after a 'href=' which needs to be after an '<a' tag
+#then collect the text after the found quote until the closing quote
+def get_next_target (html_content ):
+    anchor_string = '<a'
+    href_String = 'href='
+    
+    start_link_quote = html_content.find('"', html_content.find(href_String, html_content.find(anchor_string))) 
+    end_link_quote = html_content.find('"', start_link_quote + 1)
+    url = html_content[start_link_quote + 1 : end_link_quote]
+    
+    return url, end_link_quote
