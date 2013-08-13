75e9d27f6bfa47e76dcffaf87370d99fc10cb235
Jacob Hooey <jacob@jacobhooey.com>
2013-08-13 00:22:52 -0400

    called the search function twice

--- index.py
+++ index.py
@@ -1,10 +1,6 @@
-
-page =('<div id="top_bin"><div id="top_content" class="width960">'
-'<div class="udacity float-left"><a href= "http://udacity.com">')
-
 #finds the position of a " (left quote) after a 'href=' which needs to be after an '<a' tag
 #then collect the text after the found quote until the closing quote
-def get_next_target (html_content ):
+def get_next_target ( html_content ):
     anchor_string = '<a'
     href_String = 'href='
     
@@ -13,3 +9,18 @@ def get_next_target (html_content ):
     url = html_content[start_link_quote + 1 : end_link_quote]
     
     return url, end_link_quote
+
+
+page =('<div id="top_bin"><div id="top_content" class="width960">'
+'<div class="udacity float-left"><a href= "http://udacity.com"><a href= "http://jacob.com">')
+
+
+url, last_quote = get_next_target(page)
+page = page[last_quote:]
+
+print (url)
+
+url, last_quote = get_next_target(page)
+page = page[last_quote:]
+
+print (url)
