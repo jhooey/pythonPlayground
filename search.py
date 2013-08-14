aa2392463a603a1ee59b979999b2a8f0daeec7f8
Jacob Hooey <jhooey@gmail.com>
2013-08-14 16:07:42 -0400

    Imported urllib to collect the contents of a webpage and instead of printing the links I am placing them into an array

--- search.py
+++ search.py
@@ -1,7 +1,20 @@
 page =('<div id="top_bin"><div id="top_content" class="width960">'
 '<div class="udacity float-left"><a href= "http://udacity.com">')
 
+import urllib.request
 
+def get_page(url):
+    try:
+        f = urllib.request.urlopen(url)
+        response = f.read()
+        f.close()
+        return response
+    except:
+        return ""
+    return ""
+
+page = get_page('http://www.google.com')
+print (page)
 
 #finds the position of a " (left quote) after a 'href=' which needs to be after an '<a' tag
 #then collect the text after the found quote until the closing quote
@@ -21,13 +34,12 @@ def get_next_target ( html_content ):
     return url, end_link_quote
 
 
-def print_all_links(page):    
+def get_all_links(page):   
+    links = [] 
     while True:
         url, last_quote = get_next_target(page)
         if url:
-            print (url)
+            links.append(url)
             page = page[last_quote:]
         else:
-            break
-        
-print_all_links(page)
+            break      
