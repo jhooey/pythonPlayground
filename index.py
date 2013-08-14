f6861a8dbde7ad01bde90a8aa4972ebf42a75ce5
Jacob Hooey <jhooey@gmail.com>
2013-08-14 11:51:17 -0400

    Added a procedure that prints all the links in a string

--- index.py
+++ index.py
@@ -1,3 +1,8 @@
+page =('<div id="top_bin"><div id="top_content" class="width960">'
+'<div class="udacity float-left"><a href= "http://udacity.com">')
+
+
+
 #finds the position of a " (left quote) after a 'href=' which needs to be after an '<a' tag
 #then collect the text after the found quote until the closing quote
 def get_next_target ( html_content ):
@@ -5,22 +10,24 @@ def get_next_target ( html_content ):
     href_String = 'href='
     
     start_link_quote = html_content.find('"', html_content.find(href_String, html_content.find(anchor_string))) 
+    
+    #Error catch if a link is not found
+    if start_link_quote == -1:
+        return None, 0
+    
     end_link_quote = html_content.find('"', start_link_quote + 1)
     url = html_content[start_link_quote + 1 : end_link_quote]
     
     return url, end_link_quote
 
 
-page =('<div id="top_bin"><div id="top_content" class="width960">'
-'<div class="udacity float-left"><a href= "http://udacity.com"><a href= "http://jacob.com">')
-
-
-url, last_quote = get_next_target(page)
-page = page[last_quote:]
-
-print (url)
-
-url, last_quote = get_next_target(page)
-page = page[last_quote:]
-
-print (url)
+def print_all_links(page):    
+    while True:
+        url, last_quote = get_next_target(page)
+        if url:
+            print (url)
+            page = page[last_quote:]
+        else:
+            break
+        
+print_all_links(page)
